a522d198f0bf5160c4c9ee9ceeb9b236974983c3
Jarrett Mao <53239875+Jarrettsmao@users.noreply.github.com>
2025-08-19 21:40:32 -0700

    first commit, has some basic code showing imports

---
+++ index.js
@@ -0,0 +1,11 @@
+import { getPosts } from "./postController.js";
+
+console.log(getPosts());
+
+export { getPosts };
+// const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+// console.log(`Celcius to Fahrenheit: ${celciusToFahrenheit(0)}`);
+
